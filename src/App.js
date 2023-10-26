ab9373d7b4cc457449b6ef0e2a900340557a77e4
Louis Maillere <louis.maillere@gmail.com>
2023-10-26 05:31:06 +0200

    Création des fichier initiaux

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Contact from './pages/Contact/Contact';
+import Error from './pages/Error/Error';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+        <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/logo_web_welder.png';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className='header'>
+      <Link to='/' className='header_logo'>
+        <img src={logo} alt='logo'/>
+      </Link>
+      <nav className='header_nav'>
+        <Link to='/About'>About</Link>
+        <Link to='/Portfolio'>Portfolio</Link>
+        <Link to='/Contact'>Contact</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
